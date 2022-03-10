0e80b46fd4f6a29277d6b96677fec9dcf2619f48
Zuleiha Dantijani <tzuleiha@gmail.com>
2022-03-10 20:09:52 +0100

    ft-tasks

--- tracker/src/App.js
+++ tracker/src/App.js
@@ -1,6 +1,7 @@
 import { useState} from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -24,11 +25,26 @@ function App() {
     },
 ]) 
 
+//Delete task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !==
+  id))
+}
+
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id 
+  ? {...task, reminder: !task.reminder} : task))
+
+}
+
 
   return (
     <div className="container">
       <Header  />
-      <Tasks  tasks={tasks}/>
+      <AddTask />
+      {tasks.length > 0 ? <Tasks  tasks={tasks} onDelete=
+      {deleteTask} onToggle={toggleReminder}/> : ' No TASK TO DISPLAY '}
     </div>
   );
 }
